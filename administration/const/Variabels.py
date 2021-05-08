e569029e8e4c7450ee5863fc0dcbd0ddf88d8030
unknown <frlan.anton@gmail.com>
2021-05-08 15:46:42 +0200

    The First Cleansing

---
+++ administration/const/Variabels.py
@@ -0,0 +1,20 @@
+import enum
+
+
+class Variabels(enum.Enum):
+    SVEUCILISNI_DIPLOMSKI_STUDIJ = [
+        {'name': 'sveučilišni studij strojarstvo', 'role_list': [1, 2, 3]},
+        {'name': 'sveučilišni studij brodogradnja', 'role_list': [4, 5, 6]},
+        {'name': 'sveučilišni studij elektrotehnika', 'role_list': [7, 8, 9]},
+        {'name': 'sveučilišni studij računarstvo', 'role_list': [10, 11, 12]}
+    ]
+
+    SVEUCILISNI_PREDDIPLOMSKI_STUDIJ = [
+        {'name': 'sveučilišni studij strojarstvo', 'role_list': [13, 14, 15]},
+        {'name': 'sveučilišni studij brodogradnja', 'role_list': [18, 17, 16]},
+        {'name': 'sveučilišni studij elektrotehnika', 'role_list': [19, 20, 21]},
+        {'name': 'sveučilišni studij računarstvo', 'role_list': [22, 23, 24]},
+        {'name': 'stručni studij strojarstvo', 'role_list': [25, 26, 27]},
+        {'name': 'stručni studij brodogradnja', 'role_list': [28, 29, 30]},
+        {'name': 'stručni studij elektrotehnika', 'role_list': [31, 32, 33]}
+    ]
